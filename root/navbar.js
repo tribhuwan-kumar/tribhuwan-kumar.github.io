179afd291712867e7d6e0fe1489a400c85cb1911
Tribhuwan <tribhuwankumar735275@gmail.com>
2023-09-18 21:22:01 +0530

    initial commit

--- root/navbar.js
+++
@@ -1,8 +0,0 @@
-// for making navbar responsive
-
-const burgerMenu = document.querySelector('#menu');
-const navbar = document.querySelector('#navbar');
-
-burgerMenu.addEventListener('click', () => {
-  navbar.classList.toggle('open');
-});
