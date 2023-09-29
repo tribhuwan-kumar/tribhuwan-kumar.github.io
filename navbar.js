b9ff85d48386e41d6ace9207649c7020fe0866be
Tribhuwan <tribhuwankumar735275@gmail.com>
2023-09-30 00:25:06 +0530

    Bug fix

--- navbar.js
+++ navbar.js
@@ -1,8 +1,15 @@
 // for making navbar responsive
 
-const burgerMenu = document.querySelector('#menu');
+const burgerMenu = document.querySelector('.menu_icon');
 const navbar = document.querySelector('#navbar');
 
 burgerMenu.addEventListener('click', () => {
   navbar.classList.toggle('open');
 });
+
+const burgerIcon = document.querySelector(".menu_icon");
+    
+burgerIcon.addEventListener("click", function () {
+    burgerIcon.classList.toggle("active");
+  });
+
